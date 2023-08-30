dc933c225c6ba33f958663e52ee8332c047ff75a
dnjsxoghd <feelchok1234@gmail.com>
2023-08-30 23:27:22 +0900

    let var const

---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section5/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,14 @@
+let name = 'Max';
+
+function greet () {
+    let age = 30;
+    let name = 'Mauel'
+    console.log(name,age)
+}
+
+console.log(name)
+greet();
+
+var names = '123';
+var names = '1234'
+console.log(names)
