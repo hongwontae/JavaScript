53632a84d4813c673c4ecb0d5302e36f5669055c
dnjsxoghd <feelchok1234@gmail.com>
2023-08-30 23:35:44 +0900

    Hoisting

--- JavaScriptUdemy/JavaScriptCODE/JS-Section5/behind-scenes-01-starting-setup/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section5/behind-scenes-01-starting-setup/app.js
@@ -1,14 +1,23 @@
-let name = 'Max';
+// let name = 'Max';
 
-function greet () {
-    let age = 30;
-    let name = 'Mauel'
-    console.log(name,age)
-}
+// function greet () {
+//     let age = 30;
+//     let name = 'Mauel'
+//     console.log(name,age)
+// }
 
-console.log(name)
-greet();
+// console.log(name)
+// greet();
 
-var names = '123';
-var names = '1234'
-console.log(names)
+// var names = '123';
+// var names = '1234'
+// console.log(names)
+
+console.log(userName);
+console.log(userName2)
+
+var userName = 'Max'; // undefined
+let userName2 = 'Max' // Uncaught ReferenceError: Cannot access 'userName2' before initialization
+
+
+
