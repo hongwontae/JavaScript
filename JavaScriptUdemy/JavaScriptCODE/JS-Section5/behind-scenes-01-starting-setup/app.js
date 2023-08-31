2bd4201a7493b27a1d231f57e622f5fa26740ee9
dnjsxoghd <feelchok1234@gmail.com>
2023-08-31 15:01:02 +0900

    JS Engine Inter/Complier

--- JavaScriptUdemy/JavaScriptCODE/JS-Section5/behind-scenes-01-starting-setup/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section5/behind-scenes-01-starting-setup/app.js
@@ -13,11 +13,21 @@
 // var names = '1234'
 // console.log(names)
 
-console.log(userName);
-console.log(userName2)
+// console.log(userName);
+// console.log(userName2)
 
-var userName = 'Max'; // undefined
-let userName2 = 'Max' // Uncaught ReferenceError: Cannot access 'userName2' before initialization
+// var userName = 'Max'; // undefined
+// let userName2 = 'Max' // Uncaught ReferenceError: Cannot access 'userName2' before initialization
 
+// // var undefined = 5;
+// // console.log(undefined); //Uncaught ReferenceError: Cannot access 'userName2' before initialization
+
+// var names = 'KK'
+// var names = 'mm' // mm출력
+// console.log(names);
+
+'use strict';
+name1 = 16;
+console.log(name1)
 
 
