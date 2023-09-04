10ec0bd762aeb6d123d269c218e06c570d987b6f
dnjsxoghd <feelchok1234@gmail.com>
2023-09-04 19:46:00 +0900

    querySelector2

--- JavaScriptUdemy/JavaScriptCODE/JS-Section7/dom-02-basic-selection-code/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section7/dom-02-basic-selection-code/app.js
@@ -1,3 +1,4 @@
+// DOM 프로퍼티 탐구 및 변경
 const h1 = document.querySelector('ul');
 console.log(h1)
 
@@ -18,3 +19,36 @@ console.log(h6);
 
 const h7 = document.getElementsByTagName('li');
 console.log(h7);
+
+console.log('---------------------------------------------------------------------------')
+console.log('---------------------------------------------------------------------------')
+console.log('---------------------------------------------------------------------------')
+console.log('---------------------------------------------------------------------------')
+console.log('---------------------------------------------------------------------------')
+
+
+// 속성 VS 프로퍼티
+const a1 = document.querySelector('h1');
+console.log(a1);
+
+a1.style.background = 'black';
+a1.style.color = 'white';
+
+const a3 = a1.textContent;
+console.log(a3);
+a1.textContent = 'Some New Thing!!'
+
+console.log(a1.className)
+console.log(a1.id)
+
+const aa1 = document.querySelector('.list-item')
+console.log(aa1.className)
+
+aa1.className='koala3121';
+console.log(aa1.className) // css class나 id도 변경할 수 있다.
+
+const input = document.querySelector('input');
+input.className = 'inp'
+console.log(input.className)
+
+console.log(input.value) // html상에서 확인가능
