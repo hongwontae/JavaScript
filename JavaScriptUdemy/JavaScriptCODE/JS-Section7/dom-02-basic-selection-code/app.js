8f34f8f106f335cd6899da3b74c464d419bfe062
dnjsxoghd <feelchok1234@gmail.com>
2023-09-04 16:40:07 +0900

    DOM querySelector/by etc...

---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section7/dom-02-basic-selection-code/app.js
@@ -0,0 +1,20 @@
+const h1 = document.querySelector('ul');
+console.log(h1)
+
+const h2 = document.querySelector('.list-item')
+console.log(h2)
+
+const h3 = document.querySelector('#main-title')
+console.log(h3);
+
+const h4 = document.querySelectorAll('.list-item');
+console.log(h4)
+
+const h5 = document.getElementById('main-title');
+console.log(h5)
+
+const h6 = document.getElementsByClassName('list-item')
+console.log(h6);
+
+const h7 = document.getElementsByTagName('li');
+console.log(h7);
