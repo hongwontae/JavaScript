c0868507c398835ffb8557cc66bbc89af687e1ce
dnjsxoghd <feelchok1234@gmail.com>
2023-09-06 00:01:54 +0900

    Modal Start

---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section7/dom-07-selected-modal-and-button/assets/scripts/app.js
@@ -0,0 +1,14 @@
+//const addMovieModal = document.getElementById('add-modal');
+//const addMovieModal2 = document.querySelector('#add-modal');
+const addMovieModal = document.body.children[1]; // 세 가지 모두 html모달창을 선택하는 방법
+
+console.log(addMovieModal)
+
+const startAddMovieButton = document.querySelector('header button')
+const startAddMovieButton2 = document.querySelector('header').lastElementChild
+const startAddMovieButton3 = document.querySelector('header').firstElementChild // 이건 불가능
+
+
+console.log(startAddMovieButton)
+console.log(startAddMovieButton2)
+console.log(startAddMovieButton3)
