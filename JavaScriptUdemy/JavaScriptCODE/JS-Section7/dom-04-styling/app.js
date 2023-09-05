fb8358969a58c043f3753ff0f95c6c1a9269e672
dnjsxoghd <feelchok1234@gmail.com>
2023-09-05 15:08:06 +0900

    DOM Element Styling classList

--- JavaScriptUdemy/JavaScriptCODE/JS-Section7/dom-04-styling/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section7/dom-04-styling/app.js
@@ -4,18 +4,22 @@ const firstLi = ul.firstElementChild;
 console.log(firstLi);
 
 const section = document.querySelector('section');
-const button = document.querySelector('button');
 
-// section.style.backgroundColor = 'blue';
-section.className = 'red-bg';
+// section.style.background = 'blue'  // 1순위로 CSS가 적용된다.
+
+section.className = ''; // 해당 css class가 사라진다.
+section.classList = 'red-bg'
+
+const button = document.querySelector('button')
+
+// button.addEventListener('click', () => {
+//   if(section.className === 'red-bg visible'){
+//     section.className = 'red-bg invisible'
+//   } else {
+//     section.className = 'red-bg visible'
+//   }
+// })
 
 button.addEventListener('click', () => {
-  // if (section.className === 'red-bg visible') {
-  //   section.className = 'red-bg invisible';
-  // } else {
-  //   section.className = 'red-bg visible';
-  // }
-
-  // section.classList.toggle('visible');
-  section.classList.toggle('invisible');
-});
+  section.classList.toggle('invisible')
+})  
