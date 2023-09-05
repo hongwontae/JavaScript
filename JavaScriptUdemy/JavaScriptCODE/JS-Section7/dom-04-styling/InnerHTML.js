324b33d7854fb222c82e901fae495b23cfd68176
dnjsxoghd <feelchok1234@gmail.com>
2023-09-05 18:42:21 +0900

    query method

---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section7/dom-04-styling/InnerHTML.js
@@ -0,0 +1,9 @@
+const ul = document.querySelector('ul')
+console.log('innerHTML')
+console.log(ul.innerHTML)
+
+console.log('innerText')
+console.log(ul.innerText)
+
+console.log('textContent')
+console.log(ul.textContent)
