68e95ae7ddd963511677fe3c6d0deca15db49b35
dnjsxoghd <feelchok1234@gmail.com>
2023-09-05 19:18:32 +0900

    before after

--- JavaScriptUdemy/JavaScriptCODE/JS-Section7/dom-04-styling/BeforeAfter.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section7/dom-04-styling/BeforeAfter.js
@@ -1,7 +1,11 @@
 const ul = document.querySelector('ul');
 const li = document.createElement('li');
 
+const a1 = ul.children[1]
+li.textContent = 'like that'
+const a2 = a1.before(li) // 전
 
+const a3 = a1.after(li) // 후
 
 
 
