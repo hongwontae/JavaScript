3e96038c2a4958acfae0a969941a81435a542155
dnjsxoghd <feelchok1234@gmail.com>
2023-09-10 23:33:40 +0900

    JS Remind2 arrays forEach()

--- JavaScriptUdemy/JavaScriptCODE/JS-Section8-Array/arrays-remind/remind2.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section8-Array/arrays-remind/remind2.js
@@ -8,3 +8,25 @@ console.log(a2);
 const a3 = a.includes(10);
 console.log(a3);
 
+
+console.log('-----------------------------------------------------------------------------')
+console.log('forEach()')
+
+const b = [1,2,3,4,5,6,7,8,9,10]
+b.forEach((currentValue, idx, arr) => {
+    console.log(currentValue)
+});
+
+const b1 = b.forEach((currentValue, idx, arr) => {
+    console.log(currentValue)
+});
+console.log(b1); // undefined
+
+for(const b1 of b){
+    console.log(b1)
+}; // forEach와 for-of의 값은 같다.
+
+// forEach => 반복문을 쉽게 사용하기 위해 만든 배열 메서드이다.
+// currentValue => 현재 처리하는 값, idx => Index, arr => forEach가 처리하고 있는 배열
+// forEach의 반환값은 항상 undefined이다.
+// 복사된 값은 항상 새로운 값이다.
