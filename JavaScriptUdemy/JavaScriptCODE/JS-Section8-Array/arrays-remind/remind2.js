f19aaa3b2b6ebcf26a7808771ba30e688dac5305
dnjsxoghd <feelchok1234@gmail.com>
2023-09-10 23:27:17 +0900

    JS Remind2 include() method

---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section8-Array/arrays-remind/remind2.js
@@ -0,0 +1,10 @@
+console.log('includes()')
+
+const a = [1,2,3,4,5]
+const a1 = a.includes(1);
+console.log(a1)
+const a2 = a.includes(5);
+console.log(a2);
+const a3 = a.includes(10);
+console.log(a3);
+
