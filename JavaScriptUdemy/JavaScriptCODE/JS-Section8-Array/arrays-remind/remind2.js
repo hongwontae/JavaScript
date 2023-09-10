ac0f0a3ba824c86d9588f0538bd7b210be461dd1
dnjsxoghd <feelchok1234@gmail.com>
2023-09-10 23:40:49 +0900

    JS Remind2 arrays map() method

--- JavaScriptUdemy/JavaScriptCODE/JS-Section8-Array/arrays-remind/remind2.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section8-Array/arrays-remind/remind2.js
@@ -30,3 +30,21 @@ for(const b1 of b){
 // currentValue => 현재 처리하는 값, idx => Index, arr => forEach가 처리하고 있는 배열
 // forEach의 반환값은 항상 undefined이다.
 // 복사된 값은 항상 새로운 값이다.
+
+
+console.log('-----------------------------------------------------------------------------')
+console.log('map()')
+
+const c =[1,2,3,4,5,6,7,8,9,10]
+const c1 = c.map((currentValue, idx, arr)=>{
+    console.log(currentValue);
+    return currentValue
+})
+console.log(c1) 
+
+const c2 = c.map(currentValue => currentValue);
+console.log(c2); // 화살표 함수 사용
+
+// map은 변수에 반환값으로 복사된 값을 준다. 그 복사된 값은 새로운 값이기 떄문에 기존의 배열과 상호작용하지 않는다. return이 있어야 한다.
+// forEach와 map은 항상 콜백함수를 매개변수로 받는다.
+
