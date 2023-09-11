d7d32e7cee43df07d544a0a0e3ace74325eeded6
dnjsxoghd <feelchok1234@gmail.com>
2023-09-11 18:08:03 +0900

    Property 유형, Property order

--- JavaScriptUdemy/JavaScriptCODE/JS-Section9-Object/objects-04-property-names-square-brackets/assets/scripts/objects.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section9-Object/objects-04-property-names-square-brackets/assets/scripts/objects.js
@@ -1,7 +1,7 @@
 const movieList = document.getElementById('movie-list');
 
-movieList.style['background-color'] = 'red';
-movieList.style.display = 'block';
+movieList.style.backgroundColor = 'red';
+movieList.style.display = 'block'
 
 let person = {
   'first name': 'Max',
@@ -12,12 +12,3 @@ let person = {
   }
 };
 
-// ...
-
-// person.age = 31;
-delete person.age;
-// person.age = undefined;
-// person.age = null;
-person.isAdmin = true;
-
-console.log(person['first name']);
