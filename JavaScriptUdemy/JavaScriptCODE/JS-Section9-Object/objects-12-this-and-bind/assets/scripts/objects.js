da3fe2c693d7e7ea8544b77925627842368298ab
dnjsxoghd <feelchok1234@gmail.com>
2023-09-12 16:32:02 +0900

    this bind()

--- JavaScriptUdemy/JavaScriptCODE/JS-Section9-Object/objects-12-this-and-bind/assets/scripts/objects.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section9-Object/objects-12-this-and-bind/assets/scripts/objects.js
@@ -25,8 +25,10 @@ const renderMovies = (filter = '') => {
     console.log(otherProps);
     // const { title: movieTitle } = info;
     let { getFormattedTitle } = movie;
-    getFormattedTitle = getFormattedTitle.bind(movie);
-    let text = getFormattedTitle() + ' - ';
+    getFormattedTitle = getFormattedTitle.bind(movie); 
+    // bind가 없다면 this는 전역객체를 가르킨다. dot 표기법이 아니다. 일반 함수호출이기 떄문이다.
+    // bind()는 this가 참조할 객체를 알려준다. bind() 첫 번쨰 인자는 참조할 객체를 쓰면 된다. 그렇기에 메서드의 this는 movie를 참조한다.
+    let text = getFormattedTitle() + ' - '; 
     for (const key in info) {
       if (key !== 'title') {
         text = text + `${key}: ${info[key]}`;
@@ -59,7 +61,7 @@ const addMovieHandler = () => {
     getFormattedTitle() {
       console.log(this);
       return this.info.title.toUpperCase();
-    }
+    } // 메서드 축약 버전, this는 함수가 어디서 실행되었는지에 따라서 참조할 대상이 달라진다.
   };
 
   movies.push(newMovie);
