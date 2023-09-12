36be394a073db999b8599c465628e9c7cbabed85
dnjsxoghd <feelchok1234@gmail.com>
2023-09-13 00:40:19 +0900

    this summary

--- JavaScriptUdemy/JavaScriptCODE/JS-Section9-Object/JS-this/this.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section9-Object/JS-this/this.js
@@ -1,40 +1,62 @@
-// // 일반 함수 실행했을 떄의 this 작동방식
-// // => 전역객체를 가르킨다.
-// var age = 100;
-// console.log(this);
-// // 전역 객체를 가르키고 있음
-// var a ='hwt';
-// console.log(window.a);
-// // let,const를 전역 객체로 만들면 window 전역객체에 포함되지 않은 전역객체이다.
- 
-// function foo () {
-//   var age = 99;
-//   bar(age)
-// }
- 
-// function bar () {
-//   console.log(this.age);
-// }
- 
-// foo();
+function func() {
+  console.log(this);
+}
 
-// dot this
-// 앞의 있는 객체를 this가 참조한다. 없으면 window 전역객체를 참조한다.
-function foo() {
-  console.log(this.age);
-}
-var age = 100;
+func();
 
-var ken = {
-  age: 36,
-  foo: foo
+console.log(this);
+// 둘 다 window 객체를 반환한다.
+// 일반 함수로 호출하거나 this를 생으로 불렀을 떄는 window객체를 반환한다.
+
+
+// 암시적 바인딩
+console.log('암시적 바인딩')
+const obj = {
+  name: 'timegambit',
+  sayName() {
+    console.log(this.name);
+  },
+};
+
+obj.sayName(); // timegambit
+// sayname의 메서드의 this는 메서드를 호출한 객체을 따라간다. name => timegambit
+// 누가 호출했는지가 중요하다.
+
+
+// 명시적 바인딩 
+console.log('명시적 바인딩')
+function sayName1(a, b) {
+  console.log(this.name, a, b);
 }
-var wan = {
-  age: 32,
-  foo: foo
+
+const obj1 = {
+  name: 'timegambit',
+};
+
+sayName1.call(obj1, 1, 2); // timegambit 1 2
+// sayName1의 this는 obj1를 참조하라 => call(obj1)이 이를 일으킨다. 1,2는 함수의 인자로 들어간다.
+
+function sayName2(a, b) {
+  console.log(this.name, a, b);
 }
-ken.foo();  // 36
-wan.foo();  // 32
- 
-var fn = ken.foo;
-fn();	// 100 => 일반적인 함수 실행이라 this가 전역객체를 가르킨다.
+
+const obj2 = {
+  name: 'timegambit',
+};
+
+sayName2.apply(obj2, [1, 2]); // timegambit 1 2
+// 위와 같다. 다만 인자를 배열로 받아서 해당 함수의 매개변수로 사용한다.
+
+function sayName3(a, b) {
+  console.log(this.name, a, b);
+}
+
+const obj3 = {
+  name: 'timegambit',
+};
+
+const bound = sayName3.bind(obj3, 1);
+bound(3); // timegambit 1 2
+// 함수를 바로 실행시키는 것이 아니라 bind()를 대입받은 변수가 함수를 갖게 된다.
+// this는 obj3를 참조하고 매개변수로 1과 3을 받는다. bind() 두 번쨰 인자부터 다 넣어도 되고,
+// bind()를 받은 bound()에 인자를 줘도 된다. 그래서 1,3이 출력된다.
