c6b7de82b720c07dddc780e4cc524ef04d92b33f
dnjsxoghd <feelchok1234@gmail.com>
2023-09-13 23:38:15 +0900

    메서드와 this

--- JavaScriptBasic/this/this.js
+++ JavaScriptBasic/this/this.js
@@ -1,26 +1,14 @@
-const test = {
-    name : 'test1',
-    foo : {
-      bar : ()=> {
-        // 상위 스코프(전역)의 this인 window
-        console.log(this)
-      },
-    },
-    hello: function() {
-      /**
-       * 자바스크립트에서의 함수는 선언되는 동시에lexical)
-       * 자신만의 Scope(범위)를 가집니다.
-       */
-      // 해당 스코프(지역)의 this는 test
-      console.log(this)
-      const foo = {
-        bar: () => {
-          // 상위 스코프(지역)의 this인 test
-          console.log(this)
-        }
-      }
-      foo.bar()
+const a = {k : 'hwt', k1 : function(){console.log(this)}}
+a.k1();
+
+console.log('Arrow this')
+
+let obj = {
+    strValue: 'Hello',
+    normalFunc: function() {
+      let arrowFunc = () => { console.log(this) }
+      arrowFunc();
     }
   }
-  test.foo.bar()
-  test.hello()
+  
+  obj.normalFunc();
