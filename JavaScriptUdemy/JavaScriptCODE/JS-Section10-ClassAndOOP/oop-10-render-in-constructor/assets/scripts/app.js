2ad3361779e7c0a6b12159dc0e9b6c23fdf97d3b
dnjsxoghd <feelchok1234@gmail.com>
2023-09-14 18:58:05 +0900

    wait a minute

--- JavaScriptUdemy/JavaScriptCODE/JS-Section10-ClassAndOOP/oop-10-render-in-constructor/assets/scripts/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section10-ClassAndOOP/oop-10-render-in-constructor/assets/scripts/app.js
@@ -112,7 +112,7 @@ class ProductList extends Component {
   products = [
     new Product(
       'A Pillow',
-      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
+      'https://images.unsplash.com/photo-1682686578456-69ae00b0ecbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60',
       'A soft pillow!',
       19.99
     ),
