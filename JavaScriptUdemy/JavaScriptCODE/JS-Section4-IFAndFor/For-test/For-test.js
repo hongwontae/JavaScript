05504b7b1eef93794bcbc6710db2659b4b490d68
dnjsxoghd <feelchok1234@gmail.com>
2023-09-22 11:03:01 +0900

    Review

---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section4-IFAndFor/For-test/For-test.js
@@ -0,0 +1,15 @@
+for(let i=0; i<10; i++){
+    console.log(i)
+    console.log('나는 i')
+    for(let k=0; k<3; k++){
+        console.log(k);
+    }
+}
+
+let j=1;
+
+while(j<=5){
+    console.log(j)
+    j++
+}
+
