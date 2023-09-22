c5d764e0344ea647b8912e354ffd7ecf60a90159
dnjsxoghd <feelchok1234@gmail.com>
2023-09-22 17:42:51 +0900

    생성자 함수 and class function

---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section11-NewAndPrototype/prototypes-01-starting-setup/app.js
@@ -0,0 +1,25 @@
+class Person{
+    name = 'Max';
+
+    constructor(){
+        this.age = 30;
+    }
+
+    greet(){
+        console.log(`Hi i am ${this.name} and i am ${this.age}`)
+    }
+}
+
+function Person2(){
+    this.age = 30;
+    this.name = 'Max';
+    this.greet = function(){
+        console.log(`Hi My name is ${this.name}`)
+    }
+}
+
+const person = new Person();
+person.greet();
+
+const person2 = new Person2();
+person2.greet(); // 클래스는 이면에서는 함수로 이루어져 있다.
