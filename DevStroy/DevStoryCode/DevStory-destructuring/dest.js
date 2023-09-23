6e6462cb1c7d344cc300fa807534bb8b2edde09b
dnjsxoghd <feelchok1234@gmail.com>
2023-09-23 18:12:44 +0900

    array and Object Destructuring

---
+++ DevStroy/DevStoryCode/DevStory-destructuring/dest.js
@@ -0,0 +1,39 @@
+let a = {name : 'hwt', age :20, func(){console.log('My name is hwt')}}
+
+let {name, age, func, add='asd'} = a
+console.log(name);
+console.log(age);
+console.log(func);
+console.log(add);
+// 객체 구조 분해
+
+
+let a1 = [1,2,3,4,5,6,7,8,9,10];
+let [a2,a3,...a4] = a1;
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+let [,,a5] = a1
+console.log(a5); //3 
+
+
+let b = {name1 : 'asd', age1 : 20, arr:[1,2,3,4,5], name2 : 'koala3121'};
+let {name1, age1, arr, name2, name3 = 'ko'} = b;
+console.log(name3); //ko;
+
+let c = {player : 'hwt', age3 : 28, sprots : 'soccer', team : 'riverpool'};
+let {player, age3, sprots, team : GreatTeam} = c;
+console.log(GreatTeam); // riverpool
+
+
+let d = {d1: {d2 :'hwt', d3 : 20, d4 : 'Mancherster City'}}
+let {d1 : {d4}} = d;
+console.log(d4);
+
+let e = [1,2,3,4,5,[100,200,300]]
+let [one,two,,,,[...first]] = e;
+console.log(one);
+console.log(two);
+console.log(first);
+
