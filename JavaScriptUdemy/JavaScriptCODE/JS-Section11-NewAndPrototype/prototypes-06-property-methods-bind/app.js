15a7f21435a5d68cff4c117c48ab4373e70f57e4
dnjsxoghd <feelchok1234@gmail.com>
2023-09-27 00:34:13 +0900

    js class prototype

---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section11-NewAndPrototype/prototypes-06-property-methods-bind/app.js
@@ -0,0 +1,70 @@
+class AgedPerson {
+  printAge() {
+    console.log(this.age);
+  }
+}
+
+class Person {
+  name = 'Max';
+
+  constructor() {
+    // super();
+    this.age = 30;
+    // this.greet = function() { ... } => 1. 객체 내부에 메서드를 넣는방법
+  }
+
+  // greet = () => {
+  //   console.log(
+  //     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+  //   );
+  // }; 2. 객체 내부에 메서드를 넣는방법
+
+  greet() {
+    console.log(
+      'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+    );
+  }
+}
+
+// function Person() {
+//   this.age = 30;
+//   this.name = 'Max';
+// }
+
+// Person.prototype.greet = function() {
+//   console.log(
+//     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//   );
+// };
+
+// Person.describe = function() {
+//   console.log('Creating persons...');
+// }
+
+// Person.prototype = {
+//   printAge() {
+//     console.log(this.age);
+//   }
+// };
+
+// Person.prototype.printAge = function() {
+//   console.log(this.age);
+// };
+
+// console.dir(Person);
+
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p.length);
+// console.log(p.toString());
+// const p2 = new p.__proto__.constructor();
+// console.dir(Object.prototype.__proto__);
+
+const p = new Person();
+const p2 = new Person();
+p.greet();
+console.log(p);
+
+const button = document.getElementById('btn');
+button.addEventListener('click', p.greet.bind(p));
