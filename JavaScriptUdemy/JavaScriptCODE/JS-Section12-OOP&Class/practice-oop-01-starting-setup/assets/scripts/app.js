4c30c0b53b6e39c66096a02474fc7b8337fe9ed5
dnjsxoghd <feelchok1234@gmail.com>
2023-09-28 23:17:36 +0900

    method reference move

--- JavaScriptUdemy/JavaScriptCODE/JS-Section12-OOP&Class/practice-oop-01-starting-setup/assets/scripts/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section12-OOP&Class/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -4,10 +4,11 @@ class Tooltip{
 
 class ProjectItem{
 
-    constructor(id){
+    constructor(id, updateProjectListFunction){ //prjItem.id, switchProject() 이 메서드는 ProjectList를 참조한다.
         this.id = id;
+        this.updateProjectListHandler = updateProjectListFunction;
         this.connectMoreInfoButton();
-        this.connectSwitchButton();
+        this.connectSwitchButton(); // 실행한다.
     }
 
     connectMoreInfoButton(){
@@ -15,9 +16,10 @@ class ProjectItem{
     }
 
     connectSwitchButton(){
-        const projectItemElement = document.getElementById(this.id);
-        const switchButton = projectItemElement.querySelector('button:last-of-type');
-        switchButton.addEventListener('click',)
+        const projectItemElement = document.getElementById(this.id);//p1,p2
+        const switchButton = projectItemElement.querySelector('button:last-of-type'); // finish버튼, activate버튼을 선택
+        switchButton.addEventListener('click',this.updateProjectListHandler);
+        // 이 finish버튼이나 activate 버튼이 눌리면 swtichProject()메서드가 실행된다.
     }
 }
 
@@ -25,25 +27,29 @@ class ProjectList{
 
     projects = [];
 
-    constructor(type){
+    constructor(type){ // active, finished
         const prjItems = document.querySelectorAll(`#${type}-projects li`); //prjitems => nodeList
         // 해당 id를 가진 모든 리스트 아이템에 대한 css 선택자
         //console.log(prjItems);
         for(const prjItem of prjItems){
-            console.log(prjItem.id)
-            this.projects.push(new ProjectItem(prjItem.id));
-            
+            console.log(prjItem.id) // p1, p2
+            this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
+            // projectList 클래스의 projects 배열에 projectItem 객체를 생성하여 push한다.
         }
-        console.log(this.projects);
+    }
+    
+    setSwitchHandlerFunction(switchHandlerFunction){
+        this.switchHandlerFunction = switchHandlerFunction;
     }
 
     addProject(){
-
+        console.log(this);
     }
 
     switchProject(projectId){
         // const projectIndex = this.projects.findIndex(p => p.id === projectId)
         // this.projects.splice(projectIndex,1);
+        this.switchHandlerFunction(this.projects.find(p => p.id === projectId));
         this.projects = this.projects.filter(p => p.id !== projectId);
     }
 
@@ -51,8 +57,10 @@ class ProjectList{
 
 class App{
     static init() {
-        const activeProjectList = new ProjectList('active');
-        const finishedProjectList = new ProjectList('finished');
+        const activeProjectList = new ProjectList('active',); // Consturctor가 실행된다.
+        const finishedProjectList = new ProjectList('finished'); // Constructor가 실행된다.
+        activeProjectList.setSwitchHandlerFunction(finishedProjectList.addProject.bind(finishedProjectList));
+        finishedProjectList.setSwitchHandlerFunction(activeProjectList.addProject.bind(activeProjectList));
     };
 }
 
