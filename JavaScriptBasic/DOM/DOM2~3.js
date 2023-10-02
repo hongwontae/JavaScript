8b02fa2fc412f19c98dc323f1c8c43130fb7615f
dnjsxoghd <feelchok1234@gmail.com>
2023-10-02 17:38:02 +0900

    dom

---
+++ JavaScriptBasic/DOM/DOM2~3.js
@@ -0,0 +1,14 @@
+// udemy 3
+
+// 형제요소선택하기
+const a = document.querySelector('#udem1');
+const a1 = a.previousElementSibling // 전 형제 요소
+console.log(a1);
+
+const a2 = a.nextElementSibling // 앞 형제 요손
+console.log(a2);
+
+const a3 = document.querySelector('.udemy').className
+console.log(a3);
+
+
