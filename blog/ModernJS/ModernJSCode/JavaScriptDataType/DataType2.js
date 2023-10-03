11de9100b6946d584b00ca53fa4a0cb2e23ca81b
dnjsxoghd <feelchok1234@gmail.com>
2023-10-03 16:55:45 +0900

    JS Math

---
+++ blog/ModernJS/ModernJSCode/JavaScriptDataType/DataType2.js
@@ -0,0 +1,17 @@
+// 숫자형 => int와 bigInt
+
+let a = 1e9;
+console.log(a);
+
+let a1 = 2e-1;
+console.log(a1);
+
+let a2 = 300;
+console.log(typeof a2.toString()); 
+
+let a3 = 40000;
+Math.floor(a3);
+console.log(a3);
+
+let a4 = 50.12345;
+console.log(a4.toFixed(2));
