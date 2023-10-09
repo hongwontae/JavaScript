970cf0ac23251ac61fe26be8e4cf036f46581fd5
dnjsxoghd <feelchok1234@gmail.com>
2023-10-09 18:54:42 +0900

    transfer drop section drag trigger

--- JavaScriptUdemy/JavaScriptCODE/JS-Section14-Event/events-01-starting-setup/assets/scripts/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section14-Event/events-01-starting-setup/assets/scripts/app.js
@@ -105,10 +105,15 @@ class ProjectItem {
   }
 
   connectDrag(){
-    document.getElementById(this.id).addEventListener('dragstart', event => {
+    const item = document.getElementById(this.id)
+
+    item. addEventListener('dragstart', event => {
       event.dataTransfer.setData('text/plain', this.id);
       event.dataTransfer.effectAllowed = "move";
     });
+    item.addEventListener('dragend', event => {
+      console.log(event);
+    })
   }
 
   connectMoreInfoButton() {
@@ -174,7 +179,13 @@ class ProjectList {
     })
 
     list.addEventListener('drop',event => {
-      
+      const prjid = event.dataTransfer.getData('text/plain');
+      if(this.projects.find(p => p.id === prjid)){
+        return;
+      }
+      document.getElementById(prjid).querySelector('button:last-of-type').click();
+      list.parentElement.classList.remove('droppable');
+      event.preventDefault();
     })
 
 
