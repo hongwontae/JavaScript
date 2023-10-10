70bf4a1a60742316fa310074afa959b7e8ecfb17
dnjsxoghd <feelchok1234@gmail.com>
2023-10-10 17:58:33 +0900

    재귀 함수

--- JavaScriptUdemy/JavaScriptCODE/JS-Section15-HighFuctionConcept/adv-fn-01-starting-setup/app2.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section15-HighFuctionConcept/adv-fn-01-starting-setup/app2.js
@@ -1,10 +1,25 @@
-let userName = 'Max';
+// 재귀 Introduce
 
-function greetUser(){
-    let name = userName;
-    console.log('Hi '+name)
+// function powerOf(x,n){
+//     let result = 1;
+
+//     for(let i=0; i<n; i++ ){
+//         result*=x
+//     }
+
+//     return result;
+// }
+
+// console.log(powerOf(2,3));
+
+function powerOf(x,n){
+
+    // if(n===1){
+    //     return x;
+    // }
+    // return x*powerOf(x, n-1);
+
+    return n===1 ? x : x*powerOf(x,n-1); //2,2
 }
 
-userName = 'Manuel';
-
-greetUser();
+console.log(powerOf(2,3));
