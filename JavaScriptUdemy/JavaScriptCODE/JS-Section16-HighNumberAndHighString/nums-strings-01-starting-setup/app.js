d6f973fd4a3cd466ed44b00397f03b553c3b0f75
dnjsxoghd <feelchok1234@gmail.com>
2023-10-11 23:45:10 +0900

    Max, Min, Max_Value 증명

---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section16-HighNumberAndHighString/nums-strings-01-starting-setup/app.js
@@ -0,0 +1,14 @@
+let a = Number.MAX_SAFE_INTEGER;
+console.log(a);
+let a1 = Number.MIN_SAFE_INTEGER;
+console.log(a1);
+let a2 = Number.MAX_VALUE;
+console.log(a2);
+
+console.log('Math을 이용한 최대값 최소값')
+
+let a3 = Math.pow(2,53)-1;
+console.log(a3);
+
+let a4 = Math.pow(-2,53)+1
+console.log(a4);
