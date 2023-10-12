75595424a3d1c77ef38eeb376bccca6ada5564f6
dnjsxoghd <feelchok1234@gmail.com>
2023-10-12 19:50:18 +0900

    JS Stack and message Queue and browserAPI의 상관관계

--- JavaScriptUdemy/JavaScriptCODE/JS-Section17-PromiseAndCallBack/async-01-starting-setup/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section17-PromiseAndCallBack/async-01-starting-setup/app.js
@@ -9,3 +9,11 @@ button.addEventListener('click', trackUserHandler);
 // 이 리스너는 클릭이 될 떄까지 코드 실행을 멈추는 것이 아니다.
 // 이 리스너는 브라우저에서 관리되며 js는 다음 코드로 간다.
 // click이 되었을 떄 trackUserHandler을 연산하고 끝마치면 브라우저에서 여기로 돌아와 실행시킨다.
+
+let result = 0;
+
+for(let i=0; i<1000000; i++){
+  result+=i
+}
+
+console.log(result);
