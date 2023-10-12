6bdd645fc4cca3cd2308e9201989865193364b28
dnjsxoghd <feelchok1234@gmail.com>
2023-10-12 22:28:55 +0900

    비동기 안의 비동기 로직

--- JavaScriptUdemy/JavaScriptCODE/JS-Section17-PromiseAndCallBack/async-01-starting-setup/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section17-PromiseAndCallBack/async-01-starting-setup/app.js
@@ -3,10 +3,13 @@ const output = document.querySelector('p');
 
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(posData => {
-    console.log(posData)
+    setTimeout(()=>{
+      console.log(posData)
+    },2000)
   }, error => {
     console.log(error)
   })
+  setTimeout(() => {console.log('Timer Done')},0)
   console.log('Getting position');
 }
 
