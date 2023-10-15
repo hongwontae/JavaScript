c3364d0f963cbe5958e4f598a5ecddc485e56a0c
dnjsxoghd <feelchok1234@gmail.com>
2023-10-15 23:44:33 +0900

    Promise theh() and catch()

--- JavaScriptUdemy/JavaScriptCODE/JS-Section17-PromiseAndCallBack/async-05-first-promise/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section17-PromiseAndCallBack/async-05-first-promise/app.js
@@ -7,7 +7,9 @@ const getPosition = opts => {
       success => {
         resolve(success);
       },
-      error => {},
+      error => {
+        reject(error);
+      },
       opts
     );
   });
@@ -30,6 +32,9 @@ function trackUserHandler() {
     .then(posData => {
       positionData = posData;
       return setTimer(2000);
+    }).catch(err => {
+      console.log(err)
+      return 'on we go'
     })
     .then(data => {
       console.log(data, positionData);
