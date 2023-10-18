33caa80c75b1f13b361e1309126bf113df24ab79
dnjsxoghd <feelchok1234@gmail.com>
2023-10-18 16:27:03 +0900

    HTTP Start

--- JavaScriptUdemy/JavaScriptCODE/JS-Section17-PromiseAndCallBack/PromiseAndAsyncAwait/PAA2.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section17-PromiseAndCallBack/PromiseAndAsyncAwait/PAA2.js
@@ -35,37 +35,39 @@
 //   console.log(myPromise);
 // },3000)
 
-let a = Promise.resolve('Done');
-console.log(a);
+// let a = Promise.resolve('Done');
+// console.log(a);
 
-let b = new Promise((resolve, reject) => {
-  resolve('Done')
-})
-console.log(b);
+// let b = new Promise((resolve, reject) => {
+//   resolve('Done')
+// })
+// console.log(b);
 
-// let c = new Math();
-// console.log(c);
-// => 불가능하다.
-// let c = new JSON();
-// console.log(c);
-console.dir(Math);
-console.dir(String.constructor)
-console.dir(Promise)
-console.dir(window.Document);
+// // let c = new Math();
+// // console.log(c);
+// // => 불가능하다.
+// // let c = new JSON();
+// // console.log(c);
+// console.dir(Math);
+// console.dir(String.constructor)
+// console.dir(Promise)
+// console.dir(window.Document);
 
-let a1 = document.createElement('div');
-let a2 = document.createAttribute('button');
-console.dir(a1);
+// let a1 = document.createElement('div');
+// let a2 = document.createAttribute('button');
+// console.dir(a1);
 
-function Adding(name, age){
-  this.name = name;
-  this.age = age;
-}
+// function Adding(name, age){
+//   this.name = name;
+//   this.age = age;
+// }
 
-let person =  new Adding('hwt',20);
-console.dir(Adding);
-console.log(person);
+// let person =  new Adding('hwt',20);
+// console.dir(Adding);
+// console.log(person);
 
-console.dir(document);
-console.dir(String);
+// console.dir(document);
+// console.dir(String);
+
+
 
