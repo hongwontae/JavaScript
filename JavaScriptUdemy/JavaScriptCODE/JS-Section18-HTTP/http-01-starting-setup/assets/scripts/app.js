6d88c4ef1dfd15a563f7b8fecbe5f282545a7ab8
dnjsxoghd <feelchok1234@gmail.com>
2023-10-23 16:42:44 +0900

    JSON Data And Data Parse

--- JavaScriptUdemy/JavaScriptCODE/JS-Section18-HTTP/http-01-starting-setup/assets/scripts/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section18-HTTP/http-01-starting-setup/assets/scripts/app.js
@@ -1,4 +1,20 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
+
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET','https://jsonplaceholder.typicode.com/posts');
+
+xhr.responseType = 'json';
+
+xhr.addEventListener('load',()=>{
+    const listOfPosts = xhr.response;
+    console.log(listOfPosts);
+})
 xhr.send();
+
+
+
+
+
