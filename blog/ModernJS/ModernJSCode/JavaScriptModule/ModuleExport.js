06f03aedf7c1f8a89365cee485a4e73d40f4d45a
dnjsxoghd <feelchok1234@gmail.com>
2023-10-31 14:14:24 +0900

    Module Perfect Understanding

---
+++ blog/ModernJS/ModernJSCode/JavaScriptModule/ModuleExport.js
@@ -0,0 +1,8 @@
+export const arrs = [10, 20, 30, 40];
+export { arrs2, getName };
+
+const arrs2 = [100, 200, 300, 400];
+
+function getName() {
+    return "aaaaaaaaa";
+}
---
+++ blog/ModernJS/ModernJSCode/JavaScriptModule/ModuleImport.js
@@ -0,0 +1,5 @@
+import { arrs, arrs2, getName } from './mod.js';
+
+console.log(arrs);
+console.log(arrs2);
+console.log(getName());
