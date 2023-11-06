df05ee4865e9b958a4a39722a5d4e36ebb34d2aa
dnjsxoghd <feelchok1234@gmail.com>
2023-11-06 18:16:13 +0900

    JS-Section23

--- JavaScriptUdemy/JavaScriptCODE/JS-Section22-BrowserStorage/storage-01-starting-setup/cookies.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section22-BrowserStorage/storage-01-starting-setup/cookies.js
@@ -4,15 +4,17 @@ const retrBtn = document.getElementById('retrieve-btn');
 storeBtn.addEventListener('click', () => {
   const userId = 'u123';
   const user = {name: 'Max', age: 30};
-  document.cookie = `uid=${userId}; max-age=360`;
+  document.cookie = `uid=${userId}; max-age=200`;
   document.cookie = `user=${JSON.stringify(user)}`;
 });
 
 retrBtn.addEventListener('click', () => {
   console.log(document.cookie);
   const cookieData = document.cookie.split(';');
+  console.log(cookieData)
   const data = cookieData.map(i => {
     return i.trim();
   });
+  console.log(data);
   console.log(data[1].split('=')[1]); // user value
 });
