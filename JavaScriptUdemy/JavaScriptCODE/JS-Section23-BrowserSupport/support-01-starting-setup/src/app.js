a82fa602db1b03db79a327f597c15306cebbc201
dnjsxoghd <feelchok1234@gmail.com>
2023-11-06 22:36:14 +0900

    Polifil

--- JavaScriptUdemy/JavaScriptCODE/JS-Section23-BrowserSupport/support-01-starting-setup/src/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section23-BrowserSupport/support-01-starting-setup/src/app.js
@@ -1,5 +1,19 @@
-const button = document.querySelector('button');
+const button = document.querySelector("button");
+const textParagraph = document.querySelector("p");
+button.addEventListener("click", () => {
+  const text = textParagraph.textContent;
 
-button.addEventListener('click', () => {
-  // do something...
+  if(navigator.clipboard){
+    navigator.clipboard
+    .writeText(text)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  } else{
+    alert('ClipBoard API를 사용하시려면 Chrome를 사용해주시길 바랍니다.')
+  }
+
 });
