e89b682d4816938b527ba4432ceac342f1e53610
dnjsxoghd <feelchok1234@gmail.com>
2023-11-09 11:18:33 +0900

    class Review Finish

--- JavaScriptBasic/class/classTest.js
+++ JavaScriptBasic/class/classTest.js
@@ -3,14 +3,22 @@ class Parents{
         this.name = name;
         this.age = age;
     }
-    height = '10000';
+    good(){
+        console.log('HWT')
+    }
+
 }
 
 class Child extends Parents{
     constructor(name,age){
         super(name,age);
     }
+    insMethod(){
+        return super.good();
+}
 }
 
 let child = new Child('HWT',3000);
 console.log(child);
+child.insMethod();
+console.dir(Parents)
