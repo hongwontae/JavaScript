eb2f5305096341e0a87ddfd1465a8fe35d020f21
dnjsxoghd <feelchok1234@gmail.com>
2023-11-09 15:38:15 +0900

    Link APi

--- JavaScriptUdemy/JavaScriptCODE/JS-Section24-MyPlaceApp/practice-places-01-starting-setup/src/Utility/Location.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section24-MyPlaceApp/practice-places-01-starting-setup/src/Utility/Location.js
@@ -1,8 +1,20 @@
 const GOOGLE_API_KEY = 'AIzaSyBT2EEB8bYlAegJblbZslJ03q6FXL7YFfM'
 
+export async function getAddressFromCoords(coords) {
+    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${GOOGLE_API_KEY}`)
+    if (!response.ok) {
+      throw new Error('Failed to fetch address. Please try again!');
+    }
+    const data = await response.json();
+    if (data.error_message) {
+      throw new Error(data.error_message);
+    }
+    const address = data.results[0].formatted_address;
+    return address;
+  }
+
 export async function getCoordsFromAddress(address){
     const URLAddress = encodeURI(address);
-    console.log(URLAddress)
     const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${URLAddress}&key=${GOOGLE_API_KEY}`);
 
     if(!response.ok){
