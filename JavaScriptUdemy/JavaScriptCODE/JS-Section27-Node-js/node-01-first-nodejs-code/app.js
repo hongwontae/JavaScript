773f7d44407bc675d6775020c447437dcdadb4a6
dnjsxoghd <feelchok1234@gmail.com>
2023-11-18 13:47:28 +0900

    JS-Section Security

--- JavaScriptUdemy/JavaScriptCODE/JS-Section27-Node-js/node-01-first-nodejs-code/app.js
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section27-Node-js/node-01-first-nodejs-code/app.js
@@ -3,23 +3,23 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views','views');
+// app.set('view engine','ejs');
+// app.set('views','views');
 
 
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
 
-app.use((req, res, next)=>{
-    res.setHeader('Content-type', 'text/html')
-    next();
-});
+// app.use((req, res, next)=>{
+//     res.setHeader('Content-type', 'text/html')
+//     next();
+// });
 
-app.use((req, res, next)=>{
-    let userName = req.body.username || 'unKnown User';
-    res.render('index',{
-        user : userName
-    });
-});
+// app.use((req, res, next)=>{
+//     let userName = req.body.username || 'unKnown User';
+//     res.render('index',{
+//         user : userName
+//     });
+// });
 
 
 
---
+++ JavaScriptUdemy/JavaScriptCODE/JS-Section3-DebugAndTool/dev-debugging-finished/assets/scripts/test.js
@@ -0,0 +1,5 @@
+let c = String('HWT');
+c.goodGame = 'Good game';
+console.dir(c);
+
+console.log(c.goodGame);
