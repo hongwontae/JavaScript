8678e4d6b17a14d6456bc28546e7421c2d67e879
dnjsxoghd <feelchok1234@gmail.com>
2024-03-25 01:13:20 +0900

    JS

---
+++ JavaScript-Logic/JS-Tree-3-JS-Basic-3/JS-Tree-3.js
@@ -0,0 +1,19 @@
+// while 문
+let i = 10;
+while(i){
+    console.log(i)
+    i--;
+};
+
+let k = 0;
+while(k<=10){
+    console.log(k*k);
+    k++;
+}
+
+//do while
+let o = 10;
+do{
+    console.log(o)
+    o--
+}while(o)
