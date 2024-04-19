a2499179a1a097dfa70076f7a03f971966879089
dnjsxoghd <feelchok1234@gmail.com>
2024-04-19 23:15:36 +0900

    JS reviewing

---
+++ JS-Theory/JS-Class/class.js
@@ -0,0 +1,13 @@
+class Department{
+
+    cool = 'Cool'
+
+    constructor(id, age, name){
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+}
+
+const add = new Department(1, 20, 'HWT');
+console.log(add);
