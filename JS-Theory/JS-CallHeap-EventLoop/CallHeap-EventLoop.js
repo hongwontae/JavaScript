eeccd259d0fd852f8088313e5adb32cb705b2894
dnjsxoghd <feelchok1234@gmail.com>
2024-04-24 23:42:38 +0900

    JS Event-Loop addition

--- JS-Theory/JS-CallHeap-EventLoop/CallHeap-EventLoop.js
+++ JS-Theory/JS-CallHeap-EventLoop/CallHeap-EventLoop.js
@@ -1,1 +1,25 @@
-1. 
+const one = ()=>{
+    return Promise.resolve('Resolve');
+}
+
+async function cool(){    
+    console.log('cool-first');
+    const data = await one();
+    console.log(data);
+    console.log('SSS')
+}
+
+console.log('111')
+cool();
+console.log('222');
+console.log('222');
+console.log('222');
+console.log('222');
+console.log('222');
+console.log('222');
+console.log('222');
+console.log('222');
+console.log('222');
+console.log('222');
+console.log('222');
+console.log('222');
