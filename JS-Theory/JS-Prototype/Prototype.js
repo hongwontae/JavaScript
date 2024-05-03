397347356635d7966088372f876aa3833a06ed32
dnjsxoghd <feelchok1234@gmail.com>
2024-05-03 11:06:20 +0900

    Prototype

---
+++ JS-Theory/JS-Prototype/Prototype.js
@@ -0,0 +1,50 @@
+// let student = {
+//     name : 'kkk',
+//     class : 7
+// }
+
+// function Add(name, age){
+//     this.name = name;
+//     this.age = age;
+// };
+
+// const add = new Add('HWT',27);
+// const cool = new Add.prototype.constructor('Name',30);
+// console.log(cool);
+
+
+// let killing = {
+//     lll : 'lll',
+//     kkk : 'kkk'
+// }
+
+// console.log(killing.__proto__ === Object.prototype); // true
+// console.log(killing.__proto__.hasOwnProperty === Object.prototype.hasOwnProperty);
+
+
+// function MM(nn, kk){
+//     this.nn = nn;
+//     this.kk = kk;
+// }
+// const mm = new MM(11,22);
+
+// console.dir(MM.prototype);
+// console.dir(Function)
+
+
+// function Style(){
+//     this.name = 'hwt'
+// }
+
+// const aaaaa = new Style();
+
+let a = {
+    name : 'JWT'
+};
+console.dir(a);
+
+function kkkk (){
+    this.name = 'sss'
+};
+
+console.dir(Date)
